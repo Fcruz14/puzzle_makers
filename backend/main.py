91a40c596e7a71d98b29c9878feb88bc7e6c4b7a
LuckTateYB <fabianseverino100@gmail.com>
2025-10-05 12:47:27 -0500

    Agregar backend y frontend, actualizar estructura del proyecto

---
+++ backend/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from services.meteomatics_service import get_air_quality
+from services.supabase_service import insert_air_quality_data
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Backend Meteomatics + Supabase activo 🚀"}
+
+@app.get("/fetch-air-quality")
+def fetch_air_quality():
+    data = get_air_quality()
+    insert_air_quality_data(data)
+    return {"inserted": len(data)}
